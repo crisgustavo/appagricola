81b48e441cd7c817a9511bd98335962de22b2cda
crisgustavo <cris.gustavogs@gmail.com>
2024-11-18 15:44:23 -0300

    API change

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://177.107.115.204:3000',
+    baseURL: 'https://177.107.115.204:3000',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
